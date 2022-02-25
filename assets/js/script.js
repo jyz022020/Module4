f854da2032eb4eaf52196fda492b1ad5bddacaf0
Yunzhu Jin <jyz022020@gmail.com>
2022-02-25 00:35:35 -0800

    all functionality completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
 var timerEl = document.getElementById('timeleft');
 var startEl = document.querySelector("#start");
 var optionEls = document.getElementsByClassName("option");
+var submitButton = document.getElementById("submit");
+var gobackEl = document.getElementById("goback");
+var clearEl = document.getElementById("clearhighcores");
 var quizIndex = 0;
 var CORRECT = "Correct!";
 var WRONG = "Wrong";
 var timeLeft;  
+var timeInterval;
+
 
 var questions = ["question1", "question2", "question3", "question4", "question5"];
 var answerA = ["question1A", "question2A", "question3A", "question4A", "question5A"];
@@ -18,13 +23,13 @@ startEl.addEventListener("click", function(){
     quizIndex = 0;  
     hideAllpages();
     document.getElementById("quizpage").style.display="block";
-    var timeInterval = setInterval(function () {
+    timeInterval = setInterval(function () {
       if (timeLeft >= 1) {
         timerEl.textContent = 'Time left: ' + timeLeft;
         timeLeft--;
       } else {
-        clearInterval(timeInterval);
         hideAllpages();
+        initializeSignupPage();        
       }
     }, 1000);
     initializeQuizQuestion(quizIndex);
@@ -38,8 +43,6 @@ for (var i=0; i<optionEls.length; i++) {
         } else {
             result= WRONG;
             timeLeft = timeLeft - 10;
-            hideAllpages();
-            document.getElementById("signuppage").style.display = "block";
         }
         quizIndex++;
         initializeQuizQuestion(quizIndex);
@@ -52,6 +55,9 @@ for (var i=0; i<optionEls.length; i++) {
         }
         resultEl.textContent = result;
         resultEl.style.display = "block";
+        setTimeout(() => {
+            resultEl.style.display = "none";
+          }, 1000)
     });
 
 }
@@ -67,7 +73,8 @@ function hideAllpages() {
 function initializeQuizQuestion(index){
     if (index >= questions.length){
         hideAllpages();
-        document.getElementById("signuppage").style.display = "block";
+        initializeSignupPage();
+        // initializeSignupPage();
     } else {
         var currentQuestion = questions[index];
         var currentAnswerA = answerA[index];
@@ -82,5 +89,45 @@ function initializeQuizQuestion(index){
     }
     
 }
+
+function initializeSignupPage (){
+    setTimeout(() => {
+        clearInterval(timeInterval);
+      }, 1000)
+    document.getElementById("signuppage").style.display = "block";
+    document.getElementById("finalscore").textContent= "Your final score is " + timeLeft;
+}
+
+submitButton.addEventListener("click", function(){
+    var name = document.getElementById("initials").value;
+    initializeHighscorePage(name);
+})
+
+function initializeHighscorePage(name){
+    hideAllpages();
+    document.getElementById("timeleft").style.display = "none";
+    document.getElementById("history").style.display = "none";
+
+    if (name){
+        var newScore = document.createElement("li");
+        newScore.textContent = name + "-" +timeLeft;
+        document.getElementById("historyScores").appendChild(newScore);
+    }
+    document.getElementById("highscorepage").style.display = "block";
+
+}
+
+gobackEl.addEventListener("click",function(){
+    hideAllpages();
+    document.getElementById("welcomepage").style.display = "block";
+    document.getElementById("timeleft").textContent = "Time left: 0";
+    document.getElementById("timeleft").style.display = "block";
+    document.getElementById("history").style.display = "block";
+});
+
+clearEl.addEventListener("click",function(){
+    document.getElementById("historyScores").innerHTML = "";
+});
+
 
 
